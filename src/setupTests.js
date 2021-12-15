298696e2d45c20927a45076d37aef74faae0a272
Andrés Andrade <aaandrades@yahoo.com.co>
2021-12-14 20:50:05 -0500

    :white_check_mark: Adding unit test to Auth Actions

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,7 @@ import { createSerializer } from "enzyme-to-json";
 
 Enzyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
+
+// Fake test on scrollEvent
+// const spyScrollTo = jest.fn();
+// Object.defineProperty(global.window, 'scrollTo', { value: spyScrollTo });
